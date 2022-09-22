51f9964e0148eecbb8e7b9bd7f01a2d5bea70fb1
Maysey Lu <mayseylu@hotmail.com>
2022-09-22 16:45:53 -0700

    Created FindMax.java for max feature
    This class contains the method to find the maximum value of a given array.

---
+++ src/main/FindMax.java
@@ -0,0 +1,12 @@
+package main;
+
+public class FindMax {
+	public static int findMax(int[] input) {
+		int max = input[0];
+		for(int i = 1; i < input.length; i++) {
+			if(input[i] > max)
+				max = input[i];
+		}
+		return max;
+	}
+}
