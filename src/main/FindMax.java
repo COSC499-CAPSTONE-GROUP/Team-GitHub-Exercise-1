10956f4251735bca9e364b5353713bba3bb5dbd5
Maysey Lu <mayseylu@hotmail.com>
2022-09-22 16:54:55 -0700

    Updated FindMax.java with comments
    This code was updated with comments to explain the purpose/function of each line of code.

--- src/main/FindMax.java
+++ src/main/FindMax.java
@@ -3,13 +3,17 @@ package main;
 public class FindMax {
 	public static int findMax(int[] input) {
 		
+		//Initialize max value to be first element of array
 		int max = input[0];
 		
+		//Iterate through the array and compare each element to the current max value
 		for(int i = 1; i < input.length; i++) {
+			//If the comparing value is greater than the current max value, update max value
 			if(input[i] > max)
 				max = input[i];
 		}
 		
+		//Return the max value
 		return max;
 	}
 }
