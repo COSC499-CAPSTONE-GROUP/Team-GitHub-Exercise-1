38726418791bd1246b9cb951b337f7aef2e78074
Maysey Lu <mayseylu@hotmail.com>
2022-09-22 16:51:14 -0700

    Updated FindMax.java
    The code was updated to be more organized and improve readability by adding blank spaces.

--- src/main/FindMax.java
+++ src/main/FindMax.java
@@ -2,11 +2,14 @@ package main;
 
 public class FindMax {
 	public static int findMax(int[] input) {
+		
 		int max = input[0];
+		
 		for(int i = 1; i < input.length; i++) {
 			if(input[i] > max)
 				max = input[i];
 		}
+		
 		return max;
 	}
 }
